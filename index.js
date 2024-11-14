1a048de37d17445d325bc1d729fe1199140d176b
kisperal <stirmanjr@gmail.com>
2024-11-14 10:36:17 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const fastify = require('fastify')();
+
+// Define a test route
+fastify.get('/', () => {
+  return { hello: 'world' };
+});
+
+// Function to start the server
+async function startServer() {
+  try {
+    fastify.listen(process.env.PORT || 3000, '0.0.0.0');
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+startServer()
