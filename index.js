3bb0357b18afb63a6dbf9fd6ada8d5ec5ebaa675
kisperal <stirmanjr@gmail.com>
2024-11-14 11:23:20 -0500

    hjbhbhjbjhb

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ fastify.get('/', () => {
 // Function to start the server
 async function startServer() {
   try {
-    fastify.listen(process.env.PORT || 3000, '0.0.0.0');
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
   } catch (err) {
     fastify.log.error(err);
